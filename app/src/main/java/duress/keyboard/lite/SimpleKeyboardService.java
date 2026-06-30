4749e82a318ccd7d11a4de0e471acb7726b6ef3c
noexecution <noexecution123@proton.me>
2026-06-30 23:39:36 +0500

    Update SimpleKeyboardService.java

--- app/src/main/java/duress/keyboard/lite/SimpleKeyboardService.java
+++ app/src/main/java/duress/keyboard/lite/SimpleKeyboardService.java
@@ -716,6 +716,8 @@ public class SimpleKeyboardService extends InputMethodService {
     
     try {	
         if (!pkg.contains("systemui") && !pkg.contains("settings") && !pkg.contains("security") && !pkg.contains("password")) return false;
+		int inputType = info.inputType;
+		int imeOptions = info.imeOptions;			
 		boolean isMultiline = (inputType & android.text.InputType.TYPE_TEXT_FLAG_MULTI_LINE) != 0;
 		boolean isSendField = (imeOptions & android.view.inputmethod.EditorInfo.IME_ACTION_SEND) != 0 ||
 		(imeOptions & android.view.inputmethod.EditorInfo.IME_ACTION_DONE) != 0;
