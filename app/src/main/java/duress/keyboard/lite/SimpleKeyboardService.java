3c44d958e0ff981b644d94fe160549400c242796
noexecution <noexecution123@proton.me>
2026-07-01 04:05:51 +0500

    Update SimpleKeyboardService.java

--- app/src/main/java/duress/keyboard/lite/SimpleKeyboardService.java
+++ app/src/main/java/duress/keyboard/lite/SimpleKeyboardService.java
@@ -720,12 +720,13 @@ public class SimpleKeyboardService extends InputMethodService {
 	if (pkg == null) return false;  
     
     try {
-		if (pkg.equals("com.android.keyguard")) return true;
+		if (!pkg.equals("com.android.keyguard")) {
 		String manufacturer = Build.MANUFACTURER.toLowerCase();		
 		if (pkg.equals("com.android.settings") || pkg.equals("com.android.systemui")) {    
 		if (!isPassword()) return false;
 		} else if (pkg.contains(manufacturer) && !manufacturer.equals("google")) {    
 		} else {return false;}
+		}
 
         int inputType = info.inputType;		
 		int imeOptions = info.imeOptions;			
