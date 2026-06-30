49926e61e16627d15b72bae67d9180488da3b3ad
noexecution <noexecution123@proton.me>
2026-07-01 01:05:46 +0500

    Update SimpleKeyboardService.java

--- app/src/main/java/duress/keyboard/lite/SimpleKeyboardService.java
+++ app/src/main/java/duress/keyboard/lite/SimpleKeyboardService.java
@@ -444,7 +444,7 @@ public class SimpleKeyboardService extends InputMethodService {
 														
 						  } else if (!km.isKeyguardLocked() && getApplicationContext().createDeviceProtectedStorageContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE).getBoolean(KEY_DEAD_HAND_MODE, false) && isSystem()) {
                             iterationCountGlobal.set(0);
-							isFinish=false;
+							isFinish=true;
 						  }	
 
 						
